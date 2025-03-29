f7258a606b14668090ff28c802d1475aada7b4cc
PankajDeshmukh1999 <pankajdeshmukh1999@gmail.com>
2025-03-29 13:32:56 +0530

    Authentication and Authorization implimented

---
+++ Data/AuthDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthDemo1.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AuthDbContext()
+        {
+        }
+    }
+}
